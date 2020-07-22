a58c592f857125f6ee8da3d3610ed95ffafbbcec
Norris Mei <norrismei3@gmail.com>
2020-07-22 16:41:38 -0700

    Completed madlib

--- madlibs.py
+++ madlibs.py
@@ -42,6 +42,27 @@ def greet_person():
                            compliment=compliment)
 
 
+@app.route('/game')
+def show_madlib_form():
+    user_response = request.args.get("play_game")
+
+    if user_response == "no":
+        return render_template("goodbye.html")
+    else:
+        return render_template("game.html")
+
+
+@app.route('/madlibs')
+def show_madlib():
+    name = request.args.get("name")
+    color = request.args.get("color")
+    noun = request.args.get("noun")
+    adj = request.args.get("adj")
+
+    return render_template("madlibs.html", name=name, color=color,
+                            noun=noun, adj=adj)
+
+
 if __name__ == '__main__':
     # Setting debug=True gives us error messages in the browser and also
     # "reloads" our web app if we change the code.
